30dc4419b669f699822c3b8e8260f1ecaf2255ae
miracleoccurs <167530554+miracleoccurs@users.noreply.github.com>
2024-07-29 16:51:20 +0800

    Create binary_extensions.py

---
+++ jarvis/binary_extensions.py
@@ -0,0 +1,24 @@
+binary_extensions = [
+        # 安装包
+        ".apk",
+        # 编程语言
+        '.py', '.c', '.cs', '.cpp', '.java', '.sh', '.go', '.js', '.html', '.css', '.bat', '.asm',
+        # 常见的文件格式
+        '.txt', '.log', '.csv', '.json', '.yaml',
+        # 编译的可执行文件和库
+        '.exe', '.dll', '.so', '.a', '.lib', '.dylib', '.o', '.obj',
+        # 压缩档案
+        '.zip', '.tar', '.tar.gz', '.tgz', '.rar', '.7z', '.bz2', '.gz', '.xz', '.z', '.lz', '.lzma', '.lzo', '.rz', '.sz', '.dz',
+        # 特定于应用程序的文件
+        '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp',
+        # 媒体文件(不常见)
+        '.png', '.jpg', '.jpeg', '.gif', '.mp3', '.mp4', '.wav', '.flac', '.ogg', '.avi', '.mkv', '.mov', '.webm', '.wmv', '.m4a', '.aac',
+        # 虚拟机和容器映像
+        '.iso', '.vmdk', '.qcow2', '.vdi', '.vhd', '.vhdx', '.ova', '.ovf',
+        # 数据库文件
+        '.db', '.sqlite', '.mdb', '.accdb', '.frm', '.ibd', '.dbf',
+        # 与java相关的文件
+        '.jar', '.class', '.war', '.ear', '.jpi',
+        # Python字节码和包
+        '.pyc', '.pyo', '.pyd', '.egg', '.whl',
+    ]
